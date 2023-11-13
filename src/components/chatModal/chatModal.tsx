fbb22ce56cfc2c861a1a668e035729ea51883c45
Thanushen Balaskandar <thanushenbalaskandar@gmail.com>
2023-11-13 11:17:31 +0000

    added drop down menu to block user (no functionality)

--- src/components/chatModal/chatModal.tsx
+++ src/components/chatModal/chatModal.tsx
@@ -1,41 +1,55 @@
-import { useRef, useEffect } from "react"
-import ChatBubble from "../chatBubble/chatBubble"
-import './chatModal.css'
+import { useRef, useEffect } from "react";
+import ChatBubble from "../chatBubble/chatBubble";
+import "./chatModal.css";
 
-function ChatModal({data, div} : {data: any, div: any}) {
+function ChatModal({ data, div }: { data: any; div: any }) {
+  const scrollToBottom = () => {
+    div.current?.scrollIntoView({ behavior: "smooth", block: "end" });
+  };
 
-    const scrollToBottom =() => {
-      div.current?.scrollIntoView({ behavior: "smooth", block: "end"  })
-    }
+  useEffect(() => {
+    scrollToBottom();
+  }, [div, data]);
 
-    useEffect(() => {
-        scrollToBottom()
-      }, [div, data])
+  const mapMessages = (messages: any[]) => {
+    let res: { [key: string]: any[] } = {};
+    messages.forEach((item) => {
+      let date = new Date(item.time).toDateString();
+      if (res[date]) res[date] = res[date].concat([item]);
+      else res[date] = [item];
+    });
+    let final: any[] = [];
+    Object.keys(res).forEach((item) => {
+      res[item].forEach((e, i) => {
+        if (res[item][i + 1] && res[item][i + 1].time - e.time >= 20000)
+          res[item] = [
+            res[item].slice(0, i + 1),
+            "spacer",
+            res[item].slice(i + 1),
+          ].flat();
+      });
+      final = final.concat(
+        [<div className="chatDate">{item}</div>].concat(
+          res[item].map((item) => {
+            return item === "spacer" ? (
+              <div className="chatSpacer"></div>
+            ) : (
+              <ChatBubble id={item.id} rec={item.rec}>
+                {item.value}
+              </ChatBubble>
+            );
+          })
+        )
+      );
+    });
+    return final;
+  };
 
-    const mapMessages= (messages: any[]) => {
-        let res:{[key: string]: any[]} = {};
-        messages.forEach((item) => {
-            let date = new Date(item.time).toDateString();
-            if(res[date]) res[date] = res[date].concat([item])
-            else res[date]=[item]
-        })
-        let final: any[] = []
-        Object.keys(res).forEach((item) => {
-            res[item].forEach((e, i) => {
-                if(res[item][i+1] && res[item][i+1].time - e.time >= 20000) res[item] = [res[item].slice(0, i+1), "spacer", res[item].slice(i+1)].flat()
-            })
-            final = final.concat([(<div className="chatDate">{item}</div>)].concat(res[item].map(item => {
-                return item === "spacer" ? <div className="chatSpacer"></div> : <ChatBubble id = {item.id}rec={item.rec}>{item.value}</ChatBubble>
-            })))
-        })
-        return final;
-    }
-  
-   
-    
-    return <div className='chat' ref={div}>
+  return (
+    <div className="chat" ref={div}>
       {mapMessages(data)}
     </div>
+  );
 }
 
-export default ChatModal
+export default ChatModal;
--- src/components/header/header.tsx
+++ src/components/header/header.tsx
@@ -1,20 +1,40 @@
-import profilePic from './../../pp.jpg'
-import './header.css'
+import { useState } from "react";
+import profilePic from "./../../pp.jpg";
+import "./header.css";
 
-function Header() {
-return <div className="headerContainer">  <div className='header'>
-      <button className="backButton">{" <"}</button>
-          <div className='details'>
-          <img className="pp" src={profilePic}/>
+function Header({
+  open,
+  setOpen,
+  setDropDownMenuData,
+}: {
+  open: boolean;
+  setOpen: Function;
+  setDropDownMenuData: Function;
+}) {
+  const handleOpen = (e: any) => {
+    console.log("click");
+    setDropDownMenuData({
+      x: e.clientX,
+      y: e.clientY,
+    })
+    setOpen(true);
+  };
 
-<div className = "recipientName">
-  Thanushen
-</div>
-          </div>
-          <button className="backButton">{"..."}</button>
+  return (
+    <div className="headerContainer">
+      {" "}
+      <div className="header">
+        <div className="details">
+          <img className="pp" src={profilePic} />
 
-      </div>      </div>
-
+          <div className="recipientName">Thanushen</div>
+        </div>
+        <button className="backButton" onClick={handleOpen}>
+          {"..."}{" "}
+        </button>
+      </div>{" "}
+    </div>
+  );
 }
 
-export default Header
+export default Header;
